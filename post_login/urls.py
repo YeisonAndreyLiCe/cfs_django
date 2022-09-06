6e420f316d99bc340a636a743aa30a354ff50dcc
YeisonAndreyLiCe <yliscanoc@unal.edu.co>
2022-09-06 01:35:50 -0500

    setting djago file storage

--- post_login/urls.py
+++ post_login/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:id>/projects', views.projects, name='projects'),
     path('<int:id>/new_project', views.new_project, name='new_project'),
+    path('create_project', views.create_project, name='create_project'),
     path('projects_templates', views.projects_templates, name='projects_templates'),
 ]
