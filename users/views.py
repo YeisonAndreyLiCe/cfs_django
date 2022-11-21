d030cc2da760577364a1e2a6cb939b0c1fe56467
YeisonAndreyLiCe <yliscanoc@unal.edu.co>
2022-11-20 22:09:44 -0500

    setting routes in templates

--- users/views.py
+++ users/views.py
@@ -6,30 +6,29 @@ from django.http import JsonResponse
 from projects.models import Project
 import os
 from projects.documents import File, Artefact
+from django.urls import reverse
 
 # Create your views here.
 def projects(request, id):
     if 'user_id' not in request.session:
-        return redirect('/login')
+        return redirect(reverse("pre_login:login"))
     projects = Project.objects.filter(owner=request.session['user_id'])
     context = {
         'user': User.objects.get(id=request.session['user_id']),
         'projects': projects,
         'indexes': [i for i in range(len(projects))],
     }
-    return render(request, 'user_projects.html', context)  
-    #return render(request, 'create_project.html')
+    return render(request, 'users/user_projects.html', context) 
 
 
 def new_user_project(request):
     if 'user_id' not in request.session:
-        return redirect('/login')
-    return redirect('/projects/'+str(request.session['user_id'])+'/new_project')
+        return redirect(reverse("pre_login:login"))
+    return redirect(reverse("projects:new_project", args=(request.session["user_id"],)))
 
-def view_project(request, id_user, id_project):
+def view_project(request, id_user ,id_project):
     if 'user_id' not in request.session:
-        return redirect('/login')
-    #user = User.objects.get(id=request.session['user_id'])
+        return redirect(reverse("pre_login:login"))
     project = get_object_or_404(Project, id=id_project)
     requirements = File()
     todo = File()
@@ -40,4 +39,4 @@ def view_project(request, id_user, id_project):
         'requirements' : [Artefact(key, value) for key, value in requirements.items()],
         'to_dos' : [Artefact(key, value) for key, value in to_dos.items()],
     }
-    return render(request, 'view_user_project.html', context)
+    return render(request, 'users/view_user_project.html', context)
